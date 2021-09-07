24b639d8989650469eabd4065ed3c46608bb8489
Grace Tveit <grace.tveit@gmail.com>
2021-09-07 09:07:11 -0700

    Getting Started

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const {
+  models: { User },
+} = require("./db");
+const path = require("path");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.post("/api/auth", async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/auth", async (req, res, next) => {
+  try {
+    res.send(await User.byToken(req.headers.authorization));
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
